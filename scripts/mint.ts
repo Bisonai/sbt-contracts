62b9fff85fb11b912a7c2eb261db0a89edc3b596
Martin Kersner <martin@bisonai.com>
2022-08-25 11:46:26 +0900

    fix: `1_00` -> `1_000`

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -4,7 +4,7 @@ async function main() {
     const nft = await ethers.getContractAt("MyNFT", "0xfb04B3Dbd08134d81b8e024ed2392997d6ec6a2D");
 
     const [owner] = await ethers.getSigners();
-    for (let i = 0; i < 1_00; ++i) {
+    for (let i = 0; i < 1_000; ++i) {
         console.log("Minting ", i)
         const tx = await nft.safeMint(owner.address);
         await tx.wait();
