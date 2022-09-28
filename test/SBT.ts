ab56d09674fb35bf178d3dd31086109ffb4c0ca6
bayram98 <bayramguwanjow@gmail.com>
2022-09-28 14:56:37 +0900

    feat: improve and add more tests

--- test/SBT.ts
+++ test/SBT.ts
@@ -3,42 +3,55 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { PromiseOrValue } from "../typechain-types/common";
-import { SBT } from "../typechain-types/contracts/SBT";
+import { SBT } from "../typechain-types";
 
+let sbt: SBT
 describe("MySBT", async function() {
-    async function deployMySBT() {
+    beforeEach(async () => {
         const MySBT = await ethers.getContractFactory("SBT");
         const sbtName = "SBT";
         const sbtSymbol = "SBT";
         const baseURI = "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/";
-        const sbt = await MySBT.deploy(sbtName, sbtSymbol, baseURI);;
-        //const nft = await MyNFT.deploy();
-        return sbt;
-    }
+        sbt = await MySBT.deploy(sbtName, sbtSymbol, baseURI);
+    })
+    
+    it("#1 Should mint single MyNFT", async function() {
+        const [owner, account1] = await ethers.getSigners()
+        await sbt.safeMint(owner.address);
 
-    describe("Test", function() {
-        var sbt: SBT
-        it("#1 Should mint single MyNFT", async function() {
-            sbt = await loadFixture(deployMySBT);
-            const [owner] = await ethers.getSigners()
-            await sbt.safeMint(owner.address);
+        expect(await sbt.balanceOf(owner.address)).to.equal(1);
+        expect(await sbt.tokenURI(0)).to.equal("ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/0");
+    });
 
-            expect(await sbt.balanceOf(owner.address)).to.equal(1);
-            expect(await sbt.tokenURI(0)).to.equal("ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/0");
-        });
+    it("#2 Locked status should be True", async function() {
+        const [owner, account1] = await ethers.getSigners()
+        await sbt.safeMint(owner.address);
 
-        it("#2 Locked status should be True", async function() {
-            const lockStatus = await sbt.locked(0)
-            expect(lockStatus == true);
-        });
+        const lockStatus = await sbt.locked(0)
+        expect(lockStatus == true);
+    });
 
-        it("#3 Transfer should revert", async function() {
-            const [owner, account1] = await ethers.getSigners()
-            await expect(
-                sbt.transferFrom(owner.address, account1.address, 0)
-              ).to.be.reverted
-        });
+    it("#3 transferFrom should revert", async function() {
+        const [owner, account1] = await ethers.getSigners()
+        await sbt.safeMint(owner.address);
 
+        await expect(
+            sbt.transferFrom(owner.address, account1.address, 0)
+            ).to.be.reverted
+    });
+
+    it("#4 safeTransferFrom(address,address,uint256) should revert", async function() {
+        const [owner, account1] = await ethers.getSigners()
+        await expect(
+            sbt["safeTransferFrom(address,address,uint256)"](owner.address, account1.address, 0)
+            ).to.be.reverted
+    });
+    
+    it("#5 safeTransferFrom(address,address,uint256,bytes) should revert", async function() {
+        const [owner, account1] = await ethers.getSigners()
+        await expect(
+            sbt["safeTransferFrom(address,address,uint256,bytes)"](owner.address, account1.address, 0, [])
+            ).to.be.reverted
     });
 
 });
