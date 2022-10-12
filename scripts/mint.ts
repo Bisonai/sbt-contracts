985f6f61884f9d08e27e82b96a6ee5124694e8bd
bayram98 <bayramguwanjow@gmail.com>
2022-10-12 19:17:02 +0900

    chore: format fix

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -1,19 +1,19 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat'
 
 async function main() {
-    const sbt = await ethers.getContractAt("SBT", "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0");
+  const sbt = await ethers.getContractAt('SBT', '0x915b599DF941737Df3f69C7C4fB84BCE95CF21d6')
 
-    const [owner] = await ethers.getSigners();
-    for (let i = 0; i < 1; ++i) {
-        console.log("Minting ", i)
-        const tx = await sbt.safeMint(owner.address, 0);
-        await tx.wait();
-    }
+  const [owner] = await ethers.getSigners()
+  for (let i = 0; i < 1; ++i) {
+    console.log('Minting ', i)
+    const tx = await sbt.safeMint(owner.address, 0)
+    await tx.wait()
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
