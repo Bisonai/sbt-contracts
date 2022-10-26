ae45e4a80bb7dd24b98a3fdb68e7edf04d081eee
Martin Kersner <martin@bisonai.com>
2022-10-26 14:00:43 +0900

    chore: semi: false

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,17 @@
-import { HardhatUserConfig, task } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
-import '@nomiclabs/hardhat-web3';
-import '@nomiclabs/hardhat-ethers';
-import dotenv from 'dotenv';
+import { HardhatUserConfig, task } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomiclabs/hardhat-web3'
+import '@nomiclabs/hardhat-ethers'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 const commonConfig = {
   gas: 5_000_000,
   accounts: {
     mnemonic: process.env.MNEMONIC || ''
   }
-};
+}
 
 const config: HardhatUserConfig = {
   solidity: '0.8.16',
@@ -30,21 +30,21 @@ const config: HardhatUserConfig = {
       gasPrice: 250_000_000_000
     }
   }
-};
+}
 
 task('address', 'Convert mnemonic to address')
   .addParam('mnemonic', "The account's mnemonic")
   .setAction(async (taskArgs, hre) => {
-    const something = hre.ethers.Wallet.fromMnemonic(taskArgs.mnemonic);
-    console.log(something.address);
-  });
+    const something = hre.ethers.Wallet.fromMnemonic(taskArgs.mnemonic)
+    console.log(something.address)
+  })
 
 task('balance', "Prints an account's balance")
   .addParam('account', "The account's address")
   .setAction(async (taskArgs, hre) => {
-    const account = hre.web3.utils.toChecksumAddress(taskArgs.account);
-    const balance = await hre.web3.eth.getBalance(account);
-    console.log(hre.web3.utils.fromWei(balance, 'ether'), 'KLAY');
-  });
+    const account = hre.web3.utils.toChecksumAddress(taskArgs.account)
+    const balance = await hre.web3.eth.getBalance(account)
+    console.log(hre.web3.utils.fromWei(balance, 'ether'), 'KLAY')
+  })
 
-export default config;
+export default config
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,8 +11,6 @@ async function main() {
   console.log('MySBT deployed to:', sbt.address)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error)
   process.exitCode = 1
