87ae3788138c631de017afab7e2742e0bf2421f8
Martin Kersner <martin@bisonai.com>
2022-10-26 11:16:44 +0900

    chore: Reformat `hardhat.config.js`

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,46 +1,50 @@
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-web3";
-import "@nomiclabs/hardhat-ethers";
-import dotenv from "dotenv";
+import { HardhatUserConfig, task } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-web3';
+import '@nomiclabs/hardhat-ethers';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const commonConfig = {
-    gas: 5_000_000,
-    accounts: {
-        mnemonic: process.env.MNEMONIC || ""
-    }
-}
+  gas: 5_000_000,
+  accounts: {
+    mnemonic: process.env.MNEMONIC || ''
+  }
+};
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.16",
-    networks: {
-        localhost: {
-            gas: 1_400_000,
-        },
-        baobab: {
-            url: "https://api.baobab.klaytn.net:8651",
-            // url: 'https://public-node-api.klaytnapi.com/v1/baobab',
-            ...commonConfig,
-            gasPrice: 250_000_000_000
-        },
+  solidity: '0.8.16',
+  networks: {
+    localhost: {
+      gas: 1_400_000
+    },
+    baobab: {
+      url: 'https://api.baobab.klaytn.net:8651',
+      ...commonConfig,
+      gasPrice: 250_000_000_000
+    },
+    cypress: {
+      url: 'https://public-en-cypress.klaytn.net',
+      ...commonConfig,
+      gasPrice: 250_000_000_000
     }
-}
+  }
+};
 
-task("address", "Convert mnemonic to address")
-    .addParam("mnemonic", "The account's mnemonic")
-    .setAction(async (taskArgs, hre) => {
-        const something = hre.ethers.Wallet.fromMnemonic(taskArgs.mnemonic);
-        console.log(something.address);
-    });
+task('address', 'Convert mnemonic to address')
+  .addParam('mnemonic', "The account's mnemonic")
+  .setAction(async (taskArgs, hre) => {
+    const something = hre.ethers.Wallet.fromMnemonic(taskArgs.mnemonic);
+    console.log(something.address);
+  });
 
-task("balance", "Prints an account's balance")
-    .addParam("account", "The account's address")
-    .setAction(async (taskArgs, hre) => {
-        const account = hre.web3.utils.toChecksumAddress(taskArgs.account);
-        const balance = await hre.web3.eth.getBalance(account);
-        console.log(hre.web3.utils.fromWei(balance, "ether"), "ETH");
-    });
+task('balance', "Prints an account's balance")
+  .addParam('account', "The account's address")
+  .setAction(async (taskArgs, hre) => {
+    const account = hre.web3.utils.toChecksumAddress(taskArgs.account);
+    const balance = await hre.web3.eth.getBalance(account);
+    console.log(hre.web3.utils.fromWei(balance, 'ether'), 'KLAY');
+  });
 
 export default config;
